cb6353e7669bbde0da4d0635b91ca4bd811b0417
hynseok <wefwef12e@naver.com>
2023-12-20 16:59:58 +0900

    feat(components): 모달 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { DatePicker } from "@/components/DatePicker";
-import FileDownload from "@/components/FileDownload";
 import MainImage from "@/components/MainImage";
 
 export default function HomePage() {
