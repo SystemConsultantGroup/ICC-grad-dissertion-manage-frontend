ef0356e9752680d2a69c6d6c312a9a929ebc1a47
hynseok <wefwef12e@naver.com>
2023-12-21 16:23:39 +0900

    fix(components): 메인 이미지 수정사항 반영

--- src/components/MainImage.tsx
+++ src/components/MainImage.tsx
@@ -1,16 +1,12 @@
-import { Box, Image } from "@mantine/core";
+import { Flex } from "@mantine/core";
+import Image from "next/image";
+import largeTitle from "@/images/largeTitle.svg";
 
 function MainImage() {
   return (
-    <Box
-      style={{
-        padding: "20px",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Image src="/images/largeTitle.svg" alt="SKKU logo" fit="contain" w={500} h={116.4} />
-    </Box>
+    <Flex p={20} justify="center">
+      <Image src={largeTitle} alt="SKKU logo" />
+    </Flex>
   );
 }
 
