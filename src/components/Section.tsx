3ea1adc047f69736f22720a8829db277a00131db
hynseok <wefwef12e@naver.com>
2023-12-21 16:23:20 +0900

    fix(components): Section 수정사항 반영

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, BoxProps, useMantineTheme } from "@mantine/core";
+import { Box, BoxProps, MantineTheme, useMantineTheme } from "@mantine/core";
 import { ReactNode } from "react";
 
 interface Props extends BoxProps {
@@ -11,20 +11,20 @@ function Section({ children, ...props }: Props) {
   const theme = useMantineTheme();
 
   return (
-    <Box
-      component="section"
-      style={{
-        width: "100%",
-        borderRadius: 20,
-        border: `1px solid ${theme.colors.gray[2]}`,
-        background: theme.white,
-        padding: 20,
-      }}
-      {...props}
-    >
+    <Box component="section" style={SectionStyles(theme)} {...props}>
       {children}
     </Box>
   );
+}
+
+function SectionStyles(theme: MantineTheme) {
+  return {
+    width: "100%",
+    borderRadius: 20,
+    border: `1px solid ${theme.colors.gray[2]}`,
+    background: `${theme.white}`,
+    padding: 20,
+  };
 }
 
 export default Section;
