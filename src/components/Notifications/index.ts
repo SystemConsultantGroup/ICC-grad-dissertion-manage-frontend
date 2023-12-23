fbdaf0f6a428c492a6295cfe99c4de678f7f5202
hynseok <wefwef12e@naver.com>
2023-12-23 20:20:44 +0900

    feat(components): Notifications 추가

---
+++ src/components/Notifications/index.ts
@@ -0,0 +1,2 @@
+export { showNotificationError } from "@/components/Notifications/showNotificationError";
+export { showNotificationSuccess } from "@/components/Notifications/showNotificationSuccess";
