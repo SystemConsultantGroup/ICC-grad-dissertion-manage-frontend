487c6907afe91a4bcad063e607cca2947f6deeeb
hynseok <wefwef12e@naver.com>
2023-12-24 19:09:28 +0900

    feat(components): fetcher 추가

---
+++ src/components/fetcher.ts
@@ -0,0 +1,18 @@
+import { ClientAxios } from "@/components/ClientAxios";
+
+export interface FetcherArgs {
+  url: string;
+  query?: Record<string, unknown>;
+  token?: string | null;
+}
+
+export async function fetcher({ url, query, token }: FetcherArgs) {
+  return (
+    await ClientAxios.get(url, {
+      params: query,
+      headers: {
+        Authorization: token ? `Bearer ${token}` : undefined,
+      },
+    })
+  ).data;
+}
