4c031968432aa6b5d1bedd32a347cfee22985f30
hynseok <wefwef12e@naver.com>
2023-12-27 20:20:37 +0900

    feat(api): api 타입 정리

--- src/api/_types/common.ts
+++ src/api/_types/common.ts
@@ -1,22 +1,23 @@
+import { AxiosError } from "axios";
+
 export interface CommonApiResponse {
   message: string;
 }
+
+export interface CommonApiErrorResponse extends CommonApiResponse {
+  statusCode: number;
+}
+export interface ClientAxiosErrorResponse extends AxiosError<CommonApiErrorResponse> {}
 
 export interface PagedApiResponse<T> extends CommonApiResponse {
   pageNumber: number;
   pageSize: number;
   totalCount: number;
   totalPages: number;
-  content: T[];
+  contents: T[];
 }
 
-export type Role = "ADMIN" | "PROFESSOR" | "STUDENT";
-
-export interface User {
-  id: number;
-  deptId: number | null;
-  name: string;
-  email: string;
-  phone: string;
-  type: Role;
+export interface PagedQueryRequest {
+  pageNumber: number;
+  pageSize: number;
 }
