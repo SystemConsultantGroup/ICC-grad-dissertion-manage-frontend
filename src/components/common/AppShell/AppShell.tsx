ef9ecfe783f06e853c9dd2af76655402937dcaba
lhwdev <lhwdev6@outlook.com>
2023-12-28 15:46:04 +0900

    feat: 스타일 적용 - 원래 졸논에 있던 lineHeights 적용 - Card 기본 디자인을 디자인 시안과 비슷하게 정렬 - 필요한 css 색 변수 추가

--- src/components/common/AppShell/AppShell.tsx
+++ src/components/common/AppShell/AppShell.tsx
@@ -19,7 +19,7 @@ function AppShell({ children }: Props) {
     <MantineAppShell
       padding={40}
       navbar={{ width: 300, breakpoint: "sm" }}
-      styles={{ main: { background: "#FCFCFE" } }}
+      styles={{ main: { background: "var(--color-main)" } }}
       disabled={disabledAppShell}
     >
       <Navbar userType={user?.type} />
