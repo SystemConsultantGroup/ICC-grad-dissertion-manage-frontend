d45dbbaafbf31e7f22ac8ddf6a09bcf2ee92114f
hynseok <wefwef12e@naver.com>
2023-12-28 17:00:28 +0900

    fix(components): 내보내기 방식 수정

--- src/components/common/rows/index.ts
+++ src/components/common/rows/index.ts
@@ -1,3 +1,4 @@
 export { default as TextAreaRow } from "@/components/common/rows/TextAreaRow/TextAreaRow";
 export { default as TitleRow } from "@/components/common/rows/TitleRow/TitleRow";
 export { default as RowGroup } from "@/components/common/rows/RowGroup/RowGroup";
+export { default as FileRow } from "@/components/common/rows/FileRow/FileRow";
