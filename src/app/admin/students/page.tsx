73610e7c7b242bc7ef6d574ba38dac76b8aa1930
hynseok <wefwef12e@naver.com>
2023-12-28 16:30:24 +0900

    fix(components): 섹션, 파일다운로드 모듈 사용

--- src/app/admin/students/page.tsx
+++ src/app/admin/students/page.tsx
@@ -1,5 +1,5 @@
 import PageHeader from "@/components/common/PageHeader";
-import Section from "@/components/common/Section";
+import Section from "@/components/common/Section/Section";
 import { AuthSSR } from "@/api/AuthSSR";
 import TableTest from "./TableTest";
 
