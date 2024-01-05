ee2c47a2f3656bc134d3c5e523006b8220d87033
hynseok <wefwef12e@naver.com>
2024-01-06 01:24:21 +0900

    fix(components): DepartmentSelect placeholder 수정

--- src/components/common/selects/DepartmentSelect.tsx
+++ src/components/common/selects/DepartmentSelect.tsx
@@ -6,13 +6,13 @@ import useDepartments from "@/api/SWR/useDepartments";
 interface Props extends Omit<SelectProps, "data"> {}
 
 export function DepartmentSelect({ disabled, ...props }: Props) {
-  const { data, isLoading } = useDepartments();
+  const { data, isLoading, error } = useDepartments();
 
   return (
     <Select
       disabled={isLoading || disabled}
       {...props}
-      placeholder="소속을 선택해주세요"
+      placeholder={error ? "소속 불러오기 실패" : "소속을 선택해주세요"}
       data={data?.departments.map((department) => ({
         value: String(department.id),
         label: department.name,
