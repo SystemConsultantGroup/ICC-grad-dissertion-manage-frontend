4322178ecf50363ae4aa05456782ec52a0c8d78b
hynseok <wefwef12e@naver.com>
2024-01-05 12:58:30 +0900

    fix(api): students 타입 수정

--- src/api/_types/students.ts
+++ src/api/_types/students.ts
@@ -5,13 +5,14 @@ import { Phase } from "./phase";
 export interface PagedStudentsRequestQuery extends PagedQueryRequest {
   studentNumber?: string;
   name?: string;
-  phone?: number;
+  email?: string;
+  phone?: string;
   departmentId?: number;
   phaseId?: number;
   isLock?: boolean;
 }
 
-export interface StudentExcelRequestQuery {
+export interface StudentsExcelRequestQuery {
   studentNumber?: string;
   name?: string;
   phone?: number;
@@ -21,7 +22,7 @@ export interface StudentExcelRequestQuery {
 }
 
 export interface Student {
-  id: string;
+  id: number;
   loginId: string;
   name: string;
   email: string;
