6ec9e57050632e202120c699a2cacbfe17cf2440
hynseok <wefwef12e@naver.com>
2024-01-06 17:57:11 +0900

    feat(pages): 학과관리 페이지 추가

--- src/components/pages/department/DepartmentManageSection.tsx
+++ src/components/pages/department/DepartmentManageSection.tsx
@@ -3,9 +3,9 @@
 import useDepartments from "@/api/SWR/useDepartments";
 import { DepartmentTable, DepartmentTableRow } from "@/components/pages/department/DepartmentTable";
 import { Section } from "@/components/common/Section";
-import Split from "@/components/common/Split";
 import { Stack } from "@mantine/core";
-import HeaderRow from "@/components/common/rows/HeaderRow/HeaderRow";
+import { RowGroup, TitleRow } from "@/components/common/rows";
+import DeptCreateRow from "./DeptCreateRow";
 
 function DepartmentManageSection() {
   const { data, isLoading, mutate } = useDepartments();
@@ -13,18 +13,30 @@ function DepartmentManageSection() {
   return (
     <Section>
       <Stack>
-        <HeaderRow>
-          <Split>
-            <HeaderRow.Title>학과 목록</HeaderRow.Title>
-          </Split>
-        </HeaderRow>
-        <DepartmentTable>
-          {!isLoading &&
-            data?.departments.map((department, index) => {
-              const { id, name, userCount } = department;
-              return <DepartmentTableRow id={id} key={index} name={name} userCount={userCount} />;
-            })}
-        </DepartmentTable>
+        <Stack>
+          <TitleRow title="학과 목록" />
+          <DepartmentTable>
+            {!isLoading &&
+              data?.departments.map((department, index) => {
+                const { id, name, userCount } = department;
+                return (
+                  <DepartmentTableRow
+                    id={id}
+                    key={index}
+                    name={name}
+                    userCount={userCount}
+                    mutate={mutate}
+                  />
+                );
+              })}
+          </DepartmentTable>
+        </Stack>
+        <Stack gap={0}>
+          <TitleRow title="학과 추가" />
+          <RowGroup>
+            <DeptCreateRow mutate={mutate} />
+          </RowGroup>
+        </Stack>
       </Stack>
     </Section>
   );
