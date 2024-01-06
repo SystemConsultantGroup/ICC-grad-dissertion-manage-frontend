56d91618b82dc8155f7708482331e83417df5a1e
hynseok <wefwef12e@naver.com>
2024-01-06 17:58:02 +0900

    fix(pages): 시스템 일정 관리 페이지 학생 선택 삭제

--- src/components/pages/system/SystemSection.tsx
+++ src/components/pages/system/SystemSection.tsx
@@ -3,9 +3,9 @@ import { DatePicker } from "@/components/common/DatePicker";
 import { Section } from "@/components/common/Section";
 import Split from "@/components/common/Split";
 import { Button, Group, Select, Stack, Text } from "@mantine/core";
-import Row from "@/components/common/rows/BasicRow/BasicRow";
 import { RowGroup } from "@/components/common/rows";
 import HeaderRow from "@/components/common/rows/HeaderRow/HeaderRow";
+import Row from "@/components/common/rows/BasicRow/BasicRow";
 
 function SystemSection() {
   return (
@@ -27,26 +27,15 @@ function SystemSection() {
             ※ 모든 일정은 마지막날 23시 59분에 자동 처리 됩니다. 시스템 락은 2월 28일, 8월 31일에
             자동 해제 됩니다.
           </HeaderRow.Description>
-          <HeaderRow.Buttons>
-            <Button>수정하기</Button>
-          </HeaderRow.Buttons>
         </Split>
       </HeaderRow>
       <Stack gap={0}>
-        <RowGroup>
-          <Row field="학생 선택">
-            <Group gap="sm">
-              <Select />
-              <Select />
-            </Group>
-          </Row>
-        </RowGroup>
         <RowGroup>
           <Row field="예심">
             <></>
           </Row>
         </RowGroup>
-        <RowGroup withBorderBottom={false}>
+        <RowGroup>
           <Row field="논문 업로드 기간">
             <Group>
               <DatePicker type="range" />
@@ -54,7 +43,7 @@ function SystemSection() {
             </Group>
           </Row>
         </RowGroup>
-        <RowGroup withBorderBottom={false}>
+        <RowGroup>
           <Row field="심사 기간">
             <Group>
               <DatePicker type="range" />
@@ -75,7 +64,7 @@ function SystemSection() {
             <></>
           </Row>
         </RowGroup>
-        <RowGroup withBorderBottom={false}>
+        <RowGroup>
           <Row field="논문 업로드 기간">
             <Group>
               <DatePicker type="range" />
@@ -83,7 +72,7 @@ function SystemSection() {
             </Group>
           </Row>
         </RowGroup>
-        <RowGroup withBorderBottom={false}>
+        <RowGroup>
           <Row field="심사 기간">
             <Group>
               <DatePicker type="range" />
