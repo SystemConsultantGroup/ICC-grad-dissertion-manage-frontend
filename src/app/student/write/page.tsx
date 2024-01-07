6fcbfc8608d5275dce85871dbcb78163767e13d7
BestSanghyeon <qazxsw100415@gmail.com>
2024-01-07 23:22:11 +0900

    feat: SectionHeader 및 Section 컴포넌트 적용

--- src/app/student/write/page.tsx
+++ src/app/student/write/page.tsx
@@ -1,8 +1,17 @@
 import { AuthSSR } from "@/api/AuthSSR";
+import PageHeader from "@/components/common/PageHeader";
+import { Section } from "@/components/common/Section";
 import PaperSubmissionForm from "@/components/pages/PaperSubmissionForm/PaperSubmissionForm";
 
 export default async function StudentWritePage() {
   await AuthSSR({ userType: "STUDENT" });
 
-  return <PaperSubmissionForm />;
+  return (
+    <>
+      <PageHeader title="논문 투고" />
+      <Section>
+        <PaperSubmissionForm />
+      </Section>
+    </>
+  );
 }
