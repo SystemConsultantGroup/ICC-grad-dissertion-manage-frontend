eb6c36fd58fa1a42a1d8466c3ff4475ca2520f39
sera2002 <ouioui2@g.skku.edu>
2024-01-09 14:58:51 +0900

    refactor: 교수 정보 수정 페이지 폴더 이름 변경

--- src/app/admin/professors/[id]/page.tsx
+++ src/app/admin/professors/[id]/page.tsx
@@ -3,10 +3,6 @@ import AdminProfForm from "@/components/pages/AdminProfForm/AdminProfForm";
 import PageHeader from "@/components/common/PageHeader";
 import Section from "@/components/common/Section/Section";
 
-interface ProfEditPageParams {
-  id: string;
-}
-
 export default async function ProfEditPage({ params }: { params: { id: string } }) {
   await AuthSSR({ userType: "ADMIN" });
 
