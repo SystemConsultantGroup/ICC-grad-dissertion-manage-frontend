0335ee192b7b86276eebb3b688fc9dd432ae04a6
hynseok <wefwef12e@naver.com>
2024-01-13 18:50:06 +0900

    fix(components): 어드민 관련 리스트섹션들 폴더 이동 및 문구수정

--- src/app/admin/professors/page.tsx
+++ src/app/admin/professors/page.tsx
@@ -1,7 +1,7 @@
 import { AuthSSR } from "@/api/AuthSSR";
 import PageHeader from "@/components/common/PageHeader";
 import Section from "@/components/common/Section/Section";
-import { ProfessorListSection } from "@/components/pages/lists/ProfessorListSection";
+import { ProfessorListSection } from "@/components/pages/lists/admin/ProfessorListSection";
 
 export default async function ProfessorsPage() {
   await AuthSSR({ userType: "ADMIN" });
@@ -10,7 +10,7 @@ export default async function ProfessorsPage() {
     <>
       <PageHeader
         title="교수 현황 및 수정"
-        description="셀을 클릭하면 상세보기 및 수정 페이지로 이동합니다."
+        description="각 행을 클릭하면 상세보기 및 수정 페이지로 이동합니다."
       />
       <Section>
         <ProfessorListSection />
