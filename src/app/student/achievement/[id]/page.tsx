0805106936b34f2c1ebd32fd05efd05959252788
Yikanghoon <hoon5083@gmail.com>
2024-01-16 19:40:39 +0900

    feat: 논문 실적 수정 페이지 추가

--- src/app/student/achievement/[id]/page.tsx
+++ src/app/student/achievement/[id]/page.tsx
@@ -1,5 +1,16 @@
+import PageHeader from "@/components/common/PageHeader";
+import { Section } from "@/components/common/Section";
+import AchievementEditSection from "@/components/pages/achievement/AchievementEditSection";
+
 function StudentAchievementPage() {
-  return <></>;
+  return (
+    <>
+      <PageHeader title="연구실적 상세보기" />
+      <Section>
+        <AchievementEditSection />
+      </Section>
+    </>
+  );
 }
 
 export default StudentAchievementPage;
