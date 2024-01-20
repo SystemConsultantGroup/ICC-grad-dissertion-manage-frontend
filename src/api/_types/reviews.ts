988465cef6bfcadc6ab945360f5305a92fbb0da0
hynseok <wefwef12e@naver.com>
2024-01-20 19:08:38 +0900

    fix(api): api 타입 수정 및 (관리자)심사현황, 심사결과 swr훅 추가

--- src/api/_types/reviews.ts
+++ src/api/_types/reviews.ts
@@ -1,16 +1,32 @@
-import { CommonApiResponse, PagedApiResponse, PagedQueryRequest } from "./common";
+import { CommonApiResponse, PagedApiResponse, PagedQueryRequest, Stage, Status } from "./common";
 import { File } from "./file";
+import { User } from "./user";
 
-export type Stage = "PRELIMINARY" | "MAIN";
-export type Status = "UNEXAMINED" | "PASS" | "FAIL" | "PENDING";
+export type ReviewerRole = "COMMITTEE_CHAIR" | "COMMITTEE_MEMBER" | "ADVISOR";
+
+export interface Reviewer {
+  id: number;
+  role: ReviewerRole;
+  reviewerId: number;
+  processId: number;
+}
 
 export interface ThesisFile {
   id: number;
   type: string;
   createdAt: string;
   updatedAt: string;
-  fileId: string; //uuid
   file: File;
+}
+
+export interface ThesisInfo {
+  title: string;
+  abstract: string;
+  stage: Stage;
+  summary: Status;
+  studentInfo: User;
+  thesisFile: File;
+  presentationFile: File;
 }
 
 export interface Review {
@@ -20,10 +36,12 @@ export interface Review {
   stage: Stage;
   title: string;
   status: Status;
+  contentStatus: Status;
+  presentationStatus: Status;
   summary: Status;
 }
 
-export interface DetailedReview extends Omit<Review, "stage"> {
+export interface DetailedReview extends Review {
   abstract: string;
   thesisFiles: ThesisFile[];
   comment: string;
@@ -31,6 +49,14 @@ export interface DetailedReview extends Omit<Review, "stage"> {
 }
 
 export interface DetailedReviewResponse extends CommonApiResponse, DetailedReview {}
+
+export interface DetailedRevision extends Review {
+  abstract: string;
+  thesisFiles: ThesisFile[];
+  signFile: File;
+}
+
+export interface DetailedRevisionResponse extends CommonApiResponse, DetailedRevision {}
 
 export interface PagedReviewsResponse extends PagedApiResponse<Review> {}
 
@@ -40,12 +66,56 @@ export interface PagedReviewsRequestQuery extends PagedQueryRequest {
   stage?: Stage;
   title?: string;
   status?: Status;
-}
-
-export interface PagedReviewResultsRequestQuery extends PagedQueryRequest {
-  author?: string;
-  department?: string;
-  stage?: Stage;
-  title?: string;
   summary?: Status;
 }
+
+export interface ThesisReview {
+  id: number;
+  thesisInfo: ThesisInfo;
+  reviewer: User;
+  file: File;
+  contentStatus: Status;
+  presentationStatus: Status;
+  comment: string;
+  isFinal: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// GET: /v1/reviews/result/{id}, /v1/reviews/current/{id}
+export interface AdminReviewResponse extends CommonApiResponse {
+  id: number;
+  title: string;
+  student: string;
+  department: string;
+  abstract: string;
+  thesisFiles: ThesisFile[];
+  reviews: ThesisReview[];
+}
+
+export interface ThesisReviewStatus {
+  id: number;
+  contentStatus: Status;
+  presentationStatus: Status;
+  comment: string;
+  isFinal: boolean;
+  createdAt: string;
+  updatedAt: string;
+  reviewerId: number;
+  thesisInfoId: number;
+  fileId: string;
+  reviewer: User;
+}
+
+export interface AdminReviewStatus {
+  id: number;
+  student: string;
+  department: string;
+  stage: Stage;
+  title: string;
+  reviews: ThesisReviewStatus[];
+  reviewers: Reviewer[];
+}
+
+// GET: /v1/reviews/current
+export interface PagedReviewStatusResponse extends PagedApiResponse<AdminReviewStatus> {}
