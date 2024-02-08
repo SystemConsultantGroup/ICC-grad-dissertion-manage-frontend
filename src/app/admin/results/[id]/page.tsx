385d10c26b8088477778af393c0d936e34fca7fe
lhwdev <lhwdev6@outlook.com>
2024-02-08 17:05:31 +0900

    feat(pages): /admin/results/[id] API 연결

--- src/app/admin/results/[id]/page.tsx
+++ src/app/admin/results/[id]/page.tsx
@@ -1,18 +1,44 @@
-/* api 연결 필요 */
 import PageHeader from "@/components/common/PageHeader";
 import { ButtonRow, RowGroup } from "@/components/common/rows";
-import { ThesisInfo } from "@/components/pages/review/ThesisInfo/ThesisInfo";
+import { ThesisInfo, ThesisInfoData } from "@/components/pages/review/ThesisInfo/ThesisInfo";
 import { ReviewCard, ReviewList } from "@/components/pages/review/Review";
 import { ReviewReportAdmin } from "@/components/pages/review/Review/ReviewReport";
 import { Button } from "@mantine/core";
 import Link from "next/link";
+import { AuthSSR } from "@/api/AuthSSR";
+import { AdminReviewResponse } from "@/api/_types/reviews";
+import { API_ROUTES } from "@/api/apiRoute";
+import { fetcher } from "@/api/fetcher";
 
-export default function AdminReviewResultPage({ params: { id } }: { params: { id: string } }) {
+export default async function AdminReviewResultPage({
+  params: { id: reviewId },
+}: {
+  params: { id: string };
+}) {
+  const { token } = await AuthSSR({ userType: "ADMIN" });
+
+  const data = (await fetcher({
+    url: API_ROUTES.review.result.get(reviewId),
+    token,
+  })) as AdminReviewResponse;
+
+  const thesis: ThesisInfoData = {
+    title: data.title,
+    stage: data.stage,
+    studentInfo: {
+      name: data.student,
+      department: { name: data.department },
+    },
+    abstract: data.abstract,
+    thesisFile: data.thesisFiles.find((file) => file.type === "THESIS")!.file,
+    presentationFile: data.thesisFiles.find((file) => file.type === "PRESENTATION")!.file,
+  };
+
   return (
     <>
       <PageHeader title="심사 결과" />
       <ReviewCard>
-        <ThesisInfo stage="MAIN" revision />
+        <ThesisInfo thesis={thesis} revision={data} />
         <ReviewList title="심사 결과" stage="MAIN" />
         <ReviewReportAdmin />
         <RowGroup withBorderBottom={false}>
