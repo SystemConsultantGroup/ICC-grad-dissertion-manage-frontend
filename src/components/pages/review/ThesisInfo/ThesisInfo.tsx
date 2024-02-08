d1b27ca666624f47b95f9bf8ae16851af94c10a1
lhwdev <lhwdev6@outlook.com>
2024-02-08 17:09:05 +0900

    feat(components): ThesisInfo 컴포넌트 수정 - ThesisInfoData 모델 추상화

--- src/components/pages/review/ThesisInfo/ThesisInfo.tsx
+++ src/components/pages/review/ThesisInfo/ThesisInfo.tsx
@@ -1,16 +1,38 @@
 import { Badge, Stack } from "@mantine/core";
-import { BasicRow, FileRow, RowGroup, TitleRow } from "@/components/common/rows";
+import { ApiFileRow, BasicRow, RowGroup, TitleRow } from "@/components/common/rows";
+import { ThesisFile } from "@/api/_types/reviews";
+import { File } from "@/api/_types/file";
 
 export type Stage = "PRELIMINARY" | "MAIN";
 
 interface ThesisInfoProps {
-  stage?: Stage;
+  thesis: ThesisInfoData;
   isAdvisor?: boolean;
-  revision?: boolean;
+  revision?: ThesisRevisionInfoData;
   simple?: boolean;
 }
 
-export function ThesisInfo({ stage, isAdvisor, revision, simple = false }: ThesisInfoProps) {
+export interface ThesisInfoData {
+  title: string;
+  stage: Stage;
+  studentInfo: {
+    name: string;
+    department: {
+      name: string;
+    };
+  };
+  abstract: string;
+  thesisFile: File;
+  presentationFile: File;
+}
+
+export interface ThesisRevisionInfoData {
+  thesisFiles: ThesisFile[];
+}
+
+export function ThesisInfo({ thesis, isAdvisor, revision, simple = false }: ThesisInfoProps) {
+  const { stage } = thesis;
+
   return (
     <Stack gap={0}>
       <TitleRow
@@ -28,60 +50,68 @@ export function ThesisInfo({ stage, isAdvisor, revision, simple = false }: Thesi
           </>
         }
       />
-      <RowGroup>
-        <BasicRow field="논문 제목">
-          <BasicRow.Text>Design of Real-time SIFT Feature Extension</BasicRow.Text>
-        </BasicRow>
-      </RowGroup>
-      <RowGroup>
-        <BasicRow field="저자">
-          <BasicRow.Text>김광규</BasicRow.Text>
-        </BasicRow>
-      </RowGroup>
-      <RowGroup>
-        <BasicRow field="학과/전공">
-          <BasicRow.Text>test</BasicRow.Text>
-        </BasicRow>
-      </RowGroup>
-      <RowGroup>
-        <BasicRow field="논문 초록" p="14 0 14 0">
-          <BasicRow.Text>
-            A real-time hardware architecture based on scale-invariant feature transform algorithm
-            (SIFT) feature extraction with parallel technology has been introduced in this paper.
-            The proposed parallel hardware architecture could be able to extract feature via a
-            Field-Programmable Gate Array (FPGA) chip efficiently, which provided the real-time
-            performance and the similar accuracy with software implementation.
-          </BasicRow.Text>
-        </BasicRow>
-      </RowGroup>
-      {!simple && (
+      {simple ? (
         <>
           <RowGroup>
-            <FileRow field="논문 파일" name="test.pdf" url="." />
+            <BasicRow field="논문 제목">
+              <BasicRow.Text>{thesis.title}</BasicRow.Text>
+            </BasicRow>
           </RowGroup>
           <RowGroup>
-            <FileRow field="논문 발표 파일" name="test.pptx" url="." />
+            <BasicRow field="저자">
+              <BasicRow.Text>
+                {thesis.studentInfo.department.name} {thesis.studentInfo.name}
+              </BasicRow.Text>
+            </BasicRow>
+          </RowGroup>
+        </>
+      ) : (
+        <>
+          <RowGroup>
+            <BasicRow field="논문 제목">
+              <BasicRow.Text>{thesis.title}</BasicRow.Text>
+            </BasicRow>
+          </RowGroup>
+          <RowGroup>
+            <BasicRow field="저자">
+              <BasicRow.Text>{thesis.studentInfo.name}</BasicRow.Text>
+            </BasicRow>
+          </RowGroup>
+          <RowGroup>
+            <BasicRow field="학과/전공">
+              <BasicRow.Text>{thesis.studentInfo.department.name}</BasicRow.Text>
+            </BasicRow>
+          </RowGroup>
+          <RowGroup>
+            <BasicRow field="논문 초록" p="14 0 14 0">
+              <BasicRow.Text>{thesis.abstract}</BasicRow.Text>
+            </BasicRow>
+          </RowGroup>
+          <RowGroup>
+            <ApiFileRow field="논문 파일" file={thesis.thesisFile} />
+          </RowGroup>
+          <RowGroup>
+            <ApiFileRow field="논문 발표 파일" file={thesis.presentationFile} />
           </RowGroup>
         </>
       )}
-      {revision && (
-        <>
-          <RowGroup>
-            <FileRow field="수정 논문 파일" name="test.pdf" url="." />
-          </RowGroup>
-          <RowGroup>
-            <FileRow field="수정지시사항 결과보고서" fieldSize="lg" name="test.pptx" url="." />
-          </RowGroup>
-          <RowGroup>
-            <FileRow
-              field="(서명)수정지시사항 결과보고서"
-              fieldSize="lg"
-              name="test.pptx"
-              url="."
-            />
-          </RowGroup>
-        </>
-      )}
+      {revision && <Revisions revision={revision} />}
     </Stack>
+  );
+}
+
+function Revisions({ revision }: { revision: ThesisRevisionInfoData }) {
+  const thesis = revision.thesisFiles.find((file) => file.type === "THESIS");
+  const revision_report = revision.thesisFiles.find((file) => file.type === "REVISION_REPORT");
+
+  return (
+    <>
+      <RowGroup>
+        <ApiFileRow field="수정 논문 파일" file={thesis?.file} />
+      </RowGroup>
+      <RowGroup>
+        <ApiFileRow field="수정지시사항 결과보고서" file={revision_report?.file} />
+      </RowGroup>
+    </>
   );
 }
