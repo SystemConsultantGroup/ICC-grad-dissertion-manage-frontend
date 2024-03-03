02255452a0e935d551c128c34ba9e8cd241bf094
lhwdev <lhwdev6@outlook.com>
2024-03-03 15:25:14 +0900

    feat(pages): 관리자 심사 보기 페이지에서 논문 수정 구현 - /v1/reviews/current/:id, /final/:id의 id는 Thesis.id라는 점에 유의해서 파일 이름과 변수 이름들 수정 - PUT /thesis/:id를 이용해서 논문 수정

--- src/app/admin/reviews/[thesisId]/page.tsx
+++ src/app/admin/reviews/[thesisId]/page.tsx
@@ -2,25 +2,28 @@ import { Button } from "@mantine/core";
 import Link from "next/link";
 import PageHeader from "@/components/common/PageHeader";
 import { ButtonRow, RowGroup } from "@/components/common/rows";
-import { ThesisInfo, ThesisInfoData } from "@/components/pages/review/ThesisInfo/ThesisInfo";
+import { ThesisInfoData } from "@/components/pages/review/ThesisInfo/ThesisInfo";
 import { ReviewCard, ReviewList } from "@/components/pages/review/Review";
 import { AuthSSR } from "@/api/AuthSSR";
 import { API_ROUTES } from "@/api/apiRoute";
 import { AdminReviewResponse } from "@/api/_types/reviews";
 import { fetcher } from "@/api/fetcher";
+import { AdminThesisInfo } from "@/components/pages/review/ThesisInfo/AdminThesisInfo";
 
 export default async function AdminReviewPage({
-  params: { id: reviewId },
+  params: { id: thesisId },
 }: {
   params: { id: string };
 }) {
   const { token } = await AuthSSR({ userType: "ADMIN" });
 
   const data = (await fetcher({
-    url: API_ROUTES.review.current.get(reviewId),
+    url: API_ROUTES.review.current.get(thesisId),
     token,
   })) as AdminReviewResponse;
-  const thesis: ThesisInfoData = {
+
+  const thesis: ThesisInfoData & { id: number } = {
+    id: data.id,
     title: data.title,
     stage: data.stage,
     studentInfo: {
@@ -36,7 +39,7 @@ export default async function AdminReviewPage({
     <>
       <PageHeader title="심사 현황" />
       <ReviewCard>
-        <ThesisInfo thesis={thesis} />
+        <AdminThesisInfo thesis={thesis} />
         <ReviewList
           title="심사 현황"
           stage={data.stage}
